b0823a0b439f29f56d07aed89a4875150f61806f
Allyson Camp <Allysoncamp92@gmail.com>
2023-08-08 10:24:42 -0700

    add function to convert far to cel, add function with if/else statement
    Co-authored-by: Paul LeTourneau <pletourneau62@gmail.com>

--- scripts.js
+++ scripts.js
@@ -1,14 +1,25 @@
-// (0°C × 9/5) + 32 = 32°F
-// (32°F − 32) × 5/9 = 0°C
-
 function cToF(num1) {
+
   let tempF = (num1 * 9) / 5 + 32;
   window.alert("Your temperature in fahrenheit is: " + tempF);
 }
 
-// function fToC() {
-// }
+function fToC(num2) {
+  let tempF = (num2 - 32) * 5 / 9;
+  window.alert("Your temperature in celsius is: " + tempF);
+}
 
-let num1 = window.prompt("Enter a number here:");
-cToF(num1);
-// fToC();
+function whereToGo() {
+  if (ques === 1) {
+    let num1 = window.prompt("Enter degrees celsuis here:");
+    cToF(num1);
+  }
+  else {
+    let num2 = window.prompt("Enter degrees fahrenheit here:");
+    fToC(num2);
+  }
+}
+
+
+const ques = window.prompt("enter 1 for C to F, enter 2 F to C");
+whereToGo();
